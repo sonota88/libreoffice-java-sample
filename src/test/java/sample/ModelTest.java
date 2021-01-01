40bbf91a08340e6fd38f3f23e27d413f1173fa2f
sonota88 <yosiot8753@gmail.com>
2021-01-01 12:55:06 +0900

    JUnit のバージョンアップ: 4.x => 5.7.0 (pom.xml, ModelTest.java)

--- src/test/java/sample/ModelTest.java
+++ src/test/java/sample/ModelTest.java
@@ -1,13 +1,13 @@
 package sample;
 
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 public class ModelTest {
 
     Model sut;
-    
-    @Before
+
+    @BeforeEach
     public void setup() {
         sut = new Model();
     }
